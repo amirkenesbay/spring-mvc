4002078815f02b70270e8efd05fdee0f64b23f32
Amir <amirkenesbay@gmail.com>
2022-01-11 00:07:57 +0600

    Add validator for web-site field

--- src/main/java/mvc2/validation/CheckSiteValidator.java
+++ src/main/java/mvc2/validation/CheckSiteValidator.java
@@ -1,4 +1,21 @@
 package mvc2.validation;
 
-public class CheckSiteValidator {
+import com.amirkenesbay.spring.mvc.validation.CheckEmail;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+
+public class CheckSiteValidator implements ConstraintValidator<CheckSite, String> {
+    private String endOfWebSite;
+
+    @Override
+    public boolean isValid(String enteredValue, ConstraintValidatorContext constraintValidatorContext) {
+        return enteredValue.endsWith(endOfWebSite);
+    }
+
+    @Override
+    public void initialize(CheckSite checkSite) {
+        endOfWebSite = checkSite.value();
+    }
 }
